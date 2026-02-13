a77001f12b450ea192cb13975007d04100c2f83c
Andy Willis <awillis@lw-awillis-lt.ledwell.com>
2026-02-13 14:42:58 -0600

    correct typo

--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('adminNav', {
-  go: (url) => ipcRenderer.send('navigate-admin', url)
+  go: (url) => ipcRenderer.send('navigate-admin', url),
   promptSharePointTenant: (initialValue) => ipcRenderer.invoke('prompt-sharepoint-tenant', initialValue),
 });
